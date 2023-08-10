32123d02f73c4a9ee73f2f9fd9dd76068d511e8f
AggelosDiama <aggelosdiama@gmail.com>
2023-08-10 22:16:03 +0300

    made top bar to side bar with search

--- map.js
+++ map.js
@@ -14,3 +14,16 @@ var marker = new L.marker([38.246318, 21.735255], {}).addTo(map);
 // var popup = marker.bindPopup("SKATA" + marker.getLangLng()).openPopup();
 
 marker.bindPopup("Skata");
+
+fetch('sample_locations.json')
+  .then(response => response.json())
+  .then(data => {
+    // Loop through the JSON data and create markers
+    data.forEach(markerData => {
+      const marker = L.marker([markerData.latitude, markerData.longitude]).addTo(map);
+      
+      // Add a pop-up with additional information
+      marker.bindPopup(`<b>${markerData.name}</b><br>${markerData.description}`);
+    });
+  })
+  .catch(error => console.error('Error fetching or processing JSON data:', error));
